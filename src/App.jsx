fc88fb37665c39db2d6c65d350fbf66d5692b9d4
antodereq <Antek.Ulaczyk@wp.pl>
2026-05-31 22:26:44 +0200

    Wrzucenie importów do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import TaskFilter from "./components/TaskFilter";
+import TaskList from "./components/TaskList";
+import tasksList from "./data/tasks.json";
+import TaskForm from "./components/TaskForm";
 
 function App() {
+    const [tasks, setTasks] = useState(tasksList);
 
-  return (
-    <>
-      
-    </>
-  )
+    const [categories, setCategories] = useState([
+        { name: "praca", isChecked: true },
+        { name: "dom", isChecked: true },
+        { name: "nauka", isChecked: true },
+    ]);
+
+    const statuses = ["do_zrobienia", "w_trakcie", "ukonczone"];
+
+    const [selectedStatus, setSelectedStatus] = useState("do_zrobienia");
+
+    return (
+        <div className="container">
+            <h1>Menedżer zadań</h1>
+
+            <TaskForm setTasks={setTasks} />
+
+            <TaskFilter
+                categories={categories}
+                setCategories={setCategories}
+                statuses={statuses}
+                selectedStatus={selectedStatus}
+                setSelectedStatus={setSelectedStatus}
+            />
+
+            <TaskList
+                tasks={tasks}
+                setTasks={setTasks}
+                categories={categories}
+                statuses={statuses}
+                selectedStatus={selectedStatus}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
